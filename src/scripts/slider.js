945fb32ec97c1622131035ba66cbd247f188026c
Luan Pessoa <luan.pessoa.393@hotmail.com>
2023-04-02 18:48:07 -0300

    feature: create distances calcs when events were called

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -2,6 +2,12 @@ export default class Slider {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.distances = {
+      startX: 0,
+      endX: 0,
+      movement: 0,
+      moved: 0,
+    };
   }
 
   init() {
@@ -22,18 +28,31 @@ export default class Slider {
     this.onMove = this.onMove.bind(this);
   }
 
+  moveSlide(distanceX) {
+    this.distances.moved = distanceX;
+    this.slide.style.transform = `translate3d(${distanceX}px, 0, 0)`;
+  }
+
+  updatePosition(clientX) {
+    this.distances.movement = (this.distances.startX - clientX) * 1.5;
+    return this.distances.endX - this.distances.movement;
+  }
+
   onStart(event) {
     event.preventDefault();
+    this.distances.startX = event.clientX;
     this.wrapper.addEventListener('mousemove', this.onMove);
+  }
+
+  onMove(event) {
+    event.preventDefault();
+    const position = this.updatePosition(event.clientX);
+    this.moveSlide(position);
   }
 
   onEnd(event) {
     event.preventDefault();
     this.wrapper.removeEventListener('mousemove', this.onMove);
-  }
-
-  onMove(event) {
-    event.preventDefault();
-    return this;
+    this.distances.endX = this.distances.moved;
   }
 }
