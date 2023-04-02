8b5d374151b8c5123d63e7baec4b49c1d46c822e
Luan Pessoa <luan.pessoa.393@hotmail.com>
2023-04-02 19:54:01 -0300

    feature: add active class to slider items and resize function to re-render slider and define new positions

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,7 +1,10 @@
+import debounce from './debounce';
+
 export default class Slider {
-  constructor(slide, wrapper) {
+  constructor(slide, wrapper, activeClass) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.activeClass = activeClass !== undefined ? activeClass : 'slider__item--active';
     this.distances = {
       startX: 0,
       endX: 0,
@@ -18,6 +21,8 @@ export default class Slider {
     return this;
   }
 
+  // Sliders Configuration
+
   moveSlide(distanceX) {
     this.distances.moved = distanceX;
     this.slide.style.transform = `translate3d(${distanceX}px, 0, 0)`;
@@ -32,19 +37,73 @@ export default class Slider {
     this.slide.style.transition = bool ? 'transform 200ms ease-in-out' : '';
   }
 
+  slidePosition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  slidersConfig() {
+    this.items = [...this.slide.children].map((element) => ({
+      el: element,
+      position: this.slidePosition(element),
+    }));
+  }
+
+  slidesIndexNav(index) {
+    const lastIndex = this.items.length - 1;
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: index,
+      next: index === lastIndex ? undefined : index + 1,
+    };
+  }
+
+  changeActiveClass() {
+    this.items.forEach((item) => item.el.classList.remove(this.activeClass));
+    this.items[this.index.active].el.classList.add(this.activeClass);
+  }
+
+  activePrevSlide() {
+    if (this.index.prev === undefined) return;
+    this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide() {
+    if (this.index.next === undefined) return;
+    this.changeSlide(this.index.next);
+  }
+
+  changeSlide(index) {
+    const activeSlide = this.items[index];
+    this.moveSlide(activeSlide.position);
+    this.slidesIndexNav(index);
+    this.distances.endX = activeSlide.position;
+    this.changeActiveClass();
+  }
+
   // Event handlers
 
   bindEvents() {
+    this.onResize = debounce(this.onResize.bind(this), 100);
     this.onStart = this.onStart.bind(this);
     this.onEnd = this.onEnd.bind(this);
     this.onMove = this.onMove.bind(this);
   }
 
   addSliderEvents() {
+    window.addEventListener('resize', this.onResize);
+
     this.wrapper.addEventListener('mousedown', this.onStart);
     this.wrapper.addEventListener('mouseup', this.onEnd);
     this.wrapper.addEventListener('touchstart', this.onStart);
     this.wrapper.addEventListener('touchend', this.onEnd);
+  }
+
+  onResize() {
+    setTimeout(() => {
+      this.slidersConfig();
+      this.changeSlide(this.index.active);
+    }, 200);
   }
 
   onStart(event) {
@@ -86,44 +145,5 @@ export default class Slider {
       return;
     }
     this.changeSlide(this.index.active);
-  }
-
-  // Sliders Configuration
-  slidePosition(slide) {
-    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
-    return -(slide.offsetLeft - margin);
-  }
-
-  slidersConfig() {
-    this.items = [...this.slide.children].map((element) => ({
-      el: element,
-      position: this.slidePosition(element),
-    }));
-  }
-
-  slidesIndexNav(index) {
-    const lastIndex = this.items.length - 1;
-    this.index = {
-      prev: index ? index - 1 : undefined,
-      active: index,
-      next: index === lastIndex ? undefined : index + 1,
-    };
-  }
-
-  activePrevSlide() {
-    if (this.index.prev === undefined) return;
-    this.changeSlide(this.index.prev);
-  }
-
-  activeNextSlide() {
-    if (this.index.next === undefined) return;
-    this.changeSlide(this.index.next);
-  }
-
-  changeSlide(index) {
-    const activeSlide = this.items[index];
-    this.moveSlide(activeSlide.position);
-    this.slidesIndexNav(index);
-    this.distances.endX = activeSlide.position;
   }
 }
